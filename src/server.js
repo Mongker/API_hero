304b438edb88e87dc744eb2c33a1d1d1a29803ad
monglv <monglv@bkav.com>
2022-03-19 11:00:24 +0700

    Fix bug

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // const express = require('express');
 const express = require('express');
 const es6Renderer = require('express-es6-template-engine');
-// const path = require('path');
+const path = require('path');
 const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
